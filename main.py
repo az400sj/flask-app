13578e009bb65de0ac01be153471f8f00d0506aa
SzymonJakubiak <szymonjakubiak@gmail.com>
2024-02-03 13:02:28 +0000

    Add time-parametrized template

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', post={"time": datetime.now()})
